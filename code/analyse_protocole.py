ea81e0a7f640817bb2c5cbc46e5919af06e51b7f
Arezki DJEMAI <arezki0927@gmail.com>
2025-10-27 15:31:14 +0100

    Update analyse_protocole.py

---
+++ code/analyse_protocole.py
@@ -0,0 +1,31 @@
+# Projet d'initiation pour le Master RESET
+# Ce script doit analyser un fichier .pcap ou .pcapng
+
+# Installation requise: pip install pyshark pandas matplotlib
+
+def analyser_paquets(chemin_fichier):
+    print(f"Analyse en cours pour le fichier : {chemin_fichier}")
+    #  le vrai code plus tard
+    pass
+
+if __name__ == "__main__":
+    #  ajouter un appel simple ici pour montrer que le script s'exécute
+    analyser_paquets("exemple.pcap")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ajout de la structure initiale 
